d35cbc7c41651de09582cda783d53b1085de0f9d
David Thomas <ThomDavi727@GoTVDSB.ca>
2020-11-10 11:48:20 -0500

    Add project files.

---
+++ Hangman Project/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Hangman_Project
+{
+    partial class frmHangman
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.imgHangman = new System.Windows.Forms.PictureBox();
+            this.lstGuessedLetters = new System.Windows.Forms.ListBox();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.txtGuess = new System.Windows.Forms.TextBox();
+            this.lblText1 = new System.Windows.Forms.Label();
+            this.lblGuesses = new System.Windows.Forms.Label();
+            this.lblWord = new System.Windows.Forms.Label();
+            this.lblHint = new System.Windows.Forms.Label();
+            this.btnHint = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imgHangman)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imgHangman
+            // 
+            this.imgHangman.Image = global::Hangman_Project.Properties.Resources.Hangman;
+            this.imgHangman.Location = new System.Drawing.Point(26, 23);
+            this.imgHangman.Name = "imgHangman";
+            this.imgHangman.Size = new System.Drawing.Size(273, 281);
+            this.imgHangman.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgHangman.TabIndex = 0;
+            this.imgHangman.TabStop = false;
+            // 
+            // lstGuessedLetters
+            // 
+            this.lstGuessedLetters.FormattingEnabled = true;
+            this.lstGuessedLetters.Location = new System.Drawing.Point(451, 12);
+            this.lstGuessedLetters.Name = "lstGuessedLetters";
+            this.lstGuessedLetters.Size = new System.Drawing.Size(185, 277);
+            this.lstGuessedLetters.TabIndex = 1;
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Font = new System.Drawing.Font("Poor Richard", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuess.Location = new System.Drawing.Point(332, 60);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(75, 23);
+            this.btnGuess.TabIndex = 2;
+            this.btnGuess.Text = "Guess";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // txtGuess
+            // 
+            this.txtGuess.Location = new System.Drawing.Point(320, 34);
+            this.txtGuess.Name = "txtGuess";
+            this.txtGuess.Size = new System.Drawing.Size(100, 20);
+            this.txtGuess.TabIndex = 3;
+            this.txtGuess.TextChanged += new System.EventHandler(this.txtGuess_TextChanged);
+            // 
+            // lblText1
+            // 
+            this.lblText1.AutoSize = true;
+            this.lblText1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblText1.Location = new System.Drawing.Point(349, 439);
+            this.lblText1.Name = "lblText1";
+            this.lblText1.Size = new System.Drawing.Size(84, 23);
+            this.lblText1.TabIndex = 9;
+            this.lblText1.Text = "Guesses:";
+            // 
+            // lblGuesses
+            // 
+            this.lblGuesses.AutoSize = true;
+            this.lblGuesses.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGuesses.Location = new System.Drawing.Point(439, 442);
+            this.lblGuesses.Name = "lblGuesses";
+            this.lblGuesses.Size = new System.Drawing.Size(18, 20);
+            this.lblGuesses.TabIndex = 10;
+            this.lblGuesses.Text = "0";
+            // 
+            // lblWord
+            // 
+            this.lblWord.AutoSize = true;
+            this.lblWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWord.Location = new System.Drawing.Point(65, 338);
+            this.lblWord.Name = "lblWord";
+            this.lblWord.Size = new System.Drawing.Size(494, 37);
+            this.lblWord.TabIndex = 11;
+            this.lblWord.Text = "_ _ _ _ _   _ _ _   _ _ _   _ _ _ _ _";
+            // 
+            // lblHint
+            // 
+            this.lblHint.AutoSize = true;
+            this.lblHint.Font = new System.Drawing.Font("Poor Richard", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHint.Location = new System.Drawing.Point(12, 422);
+            this.lblHint.Name = "lblHint";
+            this.lblHint.Size = new System.Drawing.Size(200, 57);
+            this.lblHint.TabIndex = 12;
+            this.lblHint.Text = "Video Game Character.\r\nBoth loved and feared by many.\r\nWestern Style.";
+            this.lblHint.Visible = false;
+            // 
+            // btnHint
+            // 
+            this.btnHint.Location = new System.Drawing.Point(332, 89);
+            this.btnHint.Name = "btnHint";
+            this.btnHint.Size = new System.Drawing.Size(75, 23);
+            this.btnHint.TabIndex = 13;
+            this.btnHint.Text = "Hint";
+            this.btnHint.UseVisualStyleBackColor = true;
+            this.btnHint.Click += new System.EventHandler(this.btnHint_Click);
+            // 
+            // frmHangman
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(648, 488);
+            this.Controls.Add(this.btnHint);
+            this.Controls.Add(this.lblHint);
+            this.Controls.Add(this.lblWord);
+            this.Controls.Add(this.lblGuesses);
+            this.Controls.Add(this.lblText1);
+            this.Controls.Add(this.txtGuess);
+            this.Controls.Add(this.btnGuess);
+            this.Controls.Add(this.lstGuessedLetters);
+            this.Controls.Add(this.imgHangman);
+            this.Name = "frmHangman";
+            this.Text = "Hangman";
+            ((System.ComponentModel.ISupportInitialize)(this.imgHangman)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox imgHangman;
+        private System.Windows.Forms.ListBox lstGuessedLetters;
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.TextBox txtGuess;
+        private System.Windows.Forms.Label lblText1;
+        private System.Windows.Forms.Label lblGuesses;
+        private System.Windows.Forms.Label lblWord;
+        private System.Windows.Forms.Label lblHint;
+        private System.Windows.Forms.Button btnHint;
+    }
+}
+
---
+++ Hangman Project/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hangman_Project
+{
+    public partial class frmHangman : Form
+    {
+        String str = "Dutch Van Der Linde";
+        int Guesses = 0;
+        public frmHangman()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            Guesses = Guesses + 1;
+            lblGuesses.Text = Guesses.ToString();
+        }
+
+        private void txtGuess_TextChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void btnHint_Click(object sender, EventArgs e)
+        {
+            lblHint.Visible = true;
+        }
+    }
+}
